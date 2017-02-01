2ba9041d72e7709a71f4069d9a4d76a627a4ddaf
vgenov1 <vgenov1@gmail.com>
2017-02-01 23:33:39 +0200

    first draft

---
+++ js/playfield.js
@@ -0,0 +1,57 @@
+const Playfield = (function() {
+    let playfielddCells = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    class Playfield {
+        constructor($container,pieces ) {
+            this.$container = $container;
+            this.pieces = pieces;
+
+            this.render();
+        }
+
+        render() {
+
+            for(let i = 0; i < playfielddCells.length; i++) {
+                let cols = playfielddCells[i];
+
+                for(let j = 0; j < cols.length; j++) {
+                    let $div = this.$createCell(i, j);
+
+                    this.$container.appendChild($div);
+                }
+            }
+        }
+
+        $createCell(row, col) {
+            let $div = document.createElement('div');
+            $div.setAttribute('class', 'cell');
+
+
+            return $div;
+        }
+    }
+
+    return Playfield;
+  })();
