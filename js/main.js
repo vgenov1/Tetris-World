d9acaed8c7b280db0af8de798e99c3e0b7d83298
Mlazarova <mklazarova@gmail.com>
2017-02-05 23:41:06 +0200

    Pretty design

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-const game = new Game('playfield');
+$(document).ready(function(){
+	$('.fa-leanpub').click( function () {
+			$('#test').addClass('dN');
+			$('.intro-text-box').removeClass('dN');
+		});
+
+	$('.fa-play-circle').click( function () {
+			$('#introSection').addClass('dN');
+			$('.play-section').removeClass('dN');
+			$('header').addClass('smaller-header');
+		});
+});
